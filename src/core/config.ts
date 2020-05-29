f09d71768377b82f01f42ba620d6b484e4934127
WaelMohamedKhelil <khelilwael@gmail.com>
2020-05-29 15:27:52 +0100

    reduce product per page

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,7 +1,7 @@
 import { generatePageUrl } from "./utils";
 
 export const BASE_URL = "/";
-export const PRODUCTS_PER_PAGE = 18;
+export const PRODUCTS_PER_PAGE = 6;
 export const SUPPORT_EMAIL = "support@example.com";
 export const PROVIDERS = {
   BRAINTREE: {
